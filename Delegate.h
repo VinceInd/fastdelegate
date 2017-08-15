7ec2eaa21d08e085d09a49bf26a17ede0a8cd606
Vince Indriolo <vince@devshoe.com>
2017-08-15 06:19:54 -0500

    Remove tabs

--- Delegate.h
+++ Delegate.h
@@ -7,23 +7,23 @@
 namespace delly {
 
 static_assert(sizeof((int*)nullptr) == sizeof(void(*)()),
-			  "Casting magic requires: sizeof(dataptr) < sizeof(codeptr)");
+              "Casting magic requires: sizeof(dataptr) < sizeof(codeptr)");
 
 namespace details {
 
 template <class OutputClass, class InputClass>
 inline OutputClass horrible_cast(const InputClass input){
 
-	union horrible_union{
-		OutputClass out;
-		InputClass in;
-	} u;
+    union horrible_union{
+        OutputClass out;
+        InputClass in;
+    } u;
 
-	// Callable layout must be the same size in order to work properly
-	static_assert(sizeof(u) == sizeof(InputClass)
-				&& sizeof(u) == sizeof(OutputClass));
-	u.in = input;
-	return u.out;
+    // Callable layout must be the same size in order to work properly
+    static_assert(sizeof(u) == sizeof(InputClass)
+                && sizeof(u) == sizeof(OutputClass));
+    u.in = input;
+    return u.out;
 }
 
 // Dummy class used for determining function pointer size
@@ -42,43 +42,43 @@ static const int SingleMemberFuncSize = sizeof(void (DummyClass::*)());
 // |  !=0     |  !=0*     | Static function or method call|
 // +----------+-----------+-------------------------------+
 struct DelegateStorage {
-	using DummyMemFuncType = void (details::DummyClass::*) (); // member func
+    using DummyMemFuncType = void (details::DummyClass::*) (); // member func
 
-	DelegateStorage() = default;
-	DelegateStorage(details::DummyClass* p, DummyMemFuncType f)
-		: m_this(p), m_func(f)
-	{}
+    DelegateStorage() = default;
+    DelegateStorage(details::DummyClass* p, DummyMemFuncType f)
+        : m_this(p), m_func(f)
+    {}
 
-	inline details::DummyClass *getThis() const { return m_this; }
-	inline DummyMemFuncType getMemFunc() const {
-		return reinterpret_cast<DummyMemFuncType>(m_func);
-	}
+    inline details::DummyClass *getThis() const { return m_this; }
+    inline DummyMemFuncType getMemFunc() const {
+        return reinterpret_cast<DummyMemFuncType>(m_func);
+    }
 
-	void reset() { m_this = nullptr; m_func = nullptr; }
+    void reset() { m_this = nullptr; m_func = nullptr; }
 
-	inline bool empty() const { return !m_this; }
-	inline explicit operator bool() const { return !empty(); }
-	inline bool operator! () const { return empty(); }
+    inline bool empty() const { return !m_this; }
+    inline explicit operator bool() const { return !empty(); }
+    inline bool operator! () const { return empty(); }
 
-	inline bool operator==(const DelegateStorage& o) const {
-		return m_this == o.m_this && m_func == o.m_func;
-	}
-	inline bool operator!=(const DelegateStorage& o) const {
-		return !(this->operator==(o));
-	}
+    inline bool operator==(const DelegateStorage& o) const {
+        return m_this == o.m_this && m_func == o.m_func;
+    }
+    inline bool operator!=(const DelegateStorage& o) const {
+        return !(this->operator==(o));
+    }
 
-	inline bool operator <(const DelegateStorage &o) const {
-		return (m_this != o.m_this)
-				? (m_this < o.m_this)
-				: (memcmp(&m_func, &o.m_func, sizeof(m_func)) < 0);
-	}
-	inline bool operator >(const DelegateStorage &o) const {
-		return o.operator<(*this);
-	}
+    inline bool operator <(const DelegateStorage &o) const {
+        return (m_this != o.m_this)
+                ? (m_this < o.m_this)
+                : (memcmp(&m_func, &o.m_func, sizeof(m_func)) < 0);
+    }
+    inline bool operator >(const DelegateStorage &o) const {
+        return o.operator<(*this);
+    }
 
 private:
-	details::DummyClass* m_this = nullptr;
-	DummyMemFuncType     m_func = nullptr;
+    details::DummyClass* m_this = nullptr;
+    DummyMemFuncType     m_func = nullptr;
 };
 
 namespace details {
@@ -88,60 +88,60 @@ namespace details {
 template <class DummyMemFunc, class StaticFuncPtr>
 class Closure : public DelegateStorage {
 public:
-	// Set delegate to a member function
+    // Set delegate to a member function
 
-	Closure() = default;
+    Closure() = default;
 
-	// Convert member function into a standard form
-	template <class X, class XMemFunc>
-	Closure(X *pthis, XMemFunc func)
-		: DelegateStorage(bindMemFunc(pthis, func))
-	{}
+    // Convert member function into a standard form
+    template <class X, class XMemFunc>
+    Closure(X *pthis, XMemFunc func)
+        : DelegateStorage(bindMemFunc(pthis, func))
+    {}
 
-	template <class DerivedClass, class ParentInvokerSig>
-	Closure(DerivedClass *pParent, ParentInvokerSig staticFuncInvoker,
-			StaticFuncPtr func)
-		: DelegateStorage(bindStaticFunction(pParent, staticFuncInvoker, func))
-	{}
+    template <class DerivedClass, class ParentInvokerSig>
+    Closure(DerivedClass *pParent, ParentInvokerSig staticFuncInvoker,
+            StaticFuncPtr func)
+        : DelegateStorage(bindStaticFunction(pParent, staticFuncInvoker, func))
+    {}
 
-	inline DummyMemFunc getMemFunc() const {
-		return reinterpret_cast<DummyMemFunc>(DelegateStorage::getMemFunc());
-	}
+    inline DummyMemFunc getMemFunc() const {
+        return reinterpret_cast<DummyMemFunc>(DelegateStorage::getMemFunc());
+    }
 
-	inline StaticFuncPtr getStaticFunc() const {
-		// 'Evil' cast from this pointer back to a static function pointer
-		return horrible_cast<StaticFuncPtr>(this);
-	}
+    inline StaticFuncPtr getStaticFunc() const {
+        // 'Evil' cast from this pointer back to a static function pointer
+        return horrible_cast<StaticFuncPtr>(this);
+    }
 
-	inline bool operator==(const DelegateStorage& o) const {
-		return DelegateStorage::operator==(o);
-	}
+    inline bool operator==(const DelegateStorage& o) const {
+        return DelegateStorage::operator==(o);
+    }
 
-	inline bool operator==(StaticFuncPtr func) {
-		// If not storing a static function, no valid instance pointer
-	    // will be equal to the function poitner
-		return (!func) ? empty()
-				: func == getStaticFunc();
-	}
+    inline bool operator==(StaticFuncPtr func) {
+        // If not storing a static function, no valid instance pointer
+        // will be equal to the function poitner
+        return (!func) ? empty()
+                : func == getStaticFunc();
+    }
 
 private:
-	template <class X, class XMemFunc>
-	inline static DelegateStorage bindMemFunc(X *pthis, XMemFunc func) {
-		static_assert(sizeof(func) == SingleMemberFuncSize,
-					  "Unable to bind method, member size is invalid");
-		return DelegateStorage(reinterpret_cast<DummyClass*>(pthis),
-				  reinterpret_cast<DummyMemFuncType>(func));
-	}
+    template <class X, class XMemFunc>
+    inline static DelegateStorage bindMemFunc(X *pthis, XMemFunc func) {
+        static_assert(sizeof(func) == SingleMemberFuncSize,
+                      "Unable to bind method, member size is invalid");
+        return DelegateStorage(reinterpret_cast<DummyClass*>(pthis),
+                  reinterpret_cast<DummyMemFuncType>(func));
+    }
 
-	template <class DerivedClass, class ParentInvokerSig>
-	inline static DelegateStorage bindStaticFunction(DerivedClass *pParent,
-			ParentInvokerSig staticFuncInvoker,
-			StaticFuncPtr func)
-	{
-		// 'Evil': store function pointer in 'this' pointer
-		return DelegateStorage(horrible_cast<DummyClass*>(func),
-							   reinterpret_cast<DummyMemFuncType>((func == nullptr) ? nullptr : staticFuncInvoker));
-	}
+    template <class DerivedClass, class ParentInvokerSig>
+    inline static DelegateStorage bindStaticFunction(DerivedClass *pParent,
+            ParentInvokerSig staticFuncInvoker,
+            StaticFuncPtr func)
+    {
+        // 'Evil': store function pointer in 'this' pointer
+        return DelegateStorage(horrible_cast<DummyClass*>(func),
+                               reinterpret_cast<DummyMemFuncType>((func == nullptr) ? nullptr : staticFuncInvoker));
+    }
 };
 
 } // end namespace detail
@@ -151,144 +151,144 @@ template <typename Signature> class Delegate;
 template <typename RetType, typename... Args>
 class Delegate<RetType(Args...)> {
 
-	using StaticFuncType = RetType (*) (Args...);
-	using DummyMemFuncType = void (details::DummyClass::*) (Args...);
-	using ClosureType = details::Closure<DummyMemFuncType, StaticFuncType>;
+    using StaticFuncType = RetType (*) (Args...);
+    using DummyMemFuncType = void (details::DummyClass::*) (Args...);
+    using ClosureType = details::Closure<DummyMemFuncType, StaticFuncType>;
 public:
 
-	Delegate() = default;
-	Delegate(const Delegate& o) = default;
-	Delegate(Delegate&& o) = default;
-	Delegate(const std::nullptr_t) noexcept : Delegate() {}
+    Delegate() = default;
+    Delegate(const Delegate& o) = default;
+    Delegate(Delegate&& o) = default;
+    Delegate(const std::nullptr_t) noexcept : Delegate() {}
 
-	template <class X, class Y>
-	Delegate(Y* pthis, RetType (X::* func)(Args...))
-		: m_closure(static_cast<X*>(pthis), func)
-	{}
+    template <class X, class Y>
+    Delegate(Y* pthis, RetType (X::* func)(Args...))
+        : m_closure(static_cast<X*>(pthis), func)
+    {}
 
-	template <class X, class Y>
-	Delegate(Y* pthis, RetType (X::* func)(Args...) const)
-		: m_closure(static_cast<X*>(pthis), func)
-	{}
+    template <class X, class Y>
+    Delegate(Y* pthis, RetType (X::* func)(Args...) const)
+        : m_closure(static_cast<X*>(pthis), func)
+    {}
 
-	template <class X, class Y>
-	Delegate(const Y* pthis, RetType (X::* func)(Args...) const)
-		: m_closure(const_cast<X*>(pthis), func)
-	{}
+    template <class X, class Y>
+    Delegate(const Y* pthis, RetType (X::* func)(Args...) const)
+        : m_closure(const_cast<X*>(pthis), func)
+    {}
 
-	template <class X, class Y>
-	Delegate(Y& p, RetType (X::* func)(Args...))
-		: m_closure(static_cast<X&>(p), func)
-	{}
+    template <class X, class Y>
+    Delegate(Y& p, RetType (X::* func)(Args...))
+        : m_closure(static_cast<X&>(p), func)
+    {}
 
-	template <class X, class Y>
-	Delegate(Y& p, RetType (X::* func)(Args...) const)
-		: m_closure(static_cast<X&>(p), func)
-	{}
+    template <class X, class Y>
+    Delegate(Y& p, RetType (X::* func)(Args...) const)
+        : m_closure(static_cast<X&>(p), func)
+    {}
 
-	template <class X, class Y>
-	Delegate(const Y& p, RetType (X::* func)(Args...) const)
-		: m_closure(const_cast<X&>(p), func)
-	{}
+    template <class X, class Y>
+    Delegate(const Y& p, RetType (X::* func)(Args...) const)
+        : m_closure(const_cast<X&>(p), func)
+    {}
 
-	Delegate(StaticFuncType func)
-		: m_closure(this, &Delegate::InvokeStaticFunction, func)
-	{}
+    Delegate(StaticFuncType func)
+        : m_closure(this, &Delegate::InvokeStaticFunction, func)
+    {}
 
-	// Invoke the delegate when not void
-	template <typename ... Args2>
-	RetType operator() (Args2 ... args) const {
-		details::DummyClass* obj = m_closure.getThis();
-		DummyMemFuncType func	 = m_closure.getMemFunc();
-		return (obj->*func)(args...);
-	}
+    // Invoke the delegate when not void
+    template <typename ... Args2>
+    RetType operator() (Args2 ... args) const {
+        details::DummyClass* obj = m_closure.getThis();
+        DummyMemFuncType func     = m_closure.getMemFunc();
+        return (obj->*func)(args...);
+    }
 
-	void clear() { m_closure.clear(); }
+    void clear() { m_closure.clear(); }
 
-	inline bool empty() const { return m_closure.empty(); }
-	inline explicit operator bool() const { return !empty(); }
-	inline bool operator! () const { return empty(); }
+    inline bool empty() const { return m_closure.empty(); }
+    inline explicit operator bool() const { return !empty(); }
+    inline bool operator! () const { return empty(); }
 
-	constexpr Delegate& operator= (Delegate& o) = default;
-	Delegate& operator = (Delegate&& o) = default;
+    constexpr Delegate& operator= (Delegate& o) = default;
+    Delegate& operator = (Delegate&& o) = default;
     Delegate& operator = (const Delegate& o) = default;
 
-	bool operator== (const Delegate& o) const { return m_closure == o.m_closure; }
-	bool operator!= (const Delegate& o) const { return m_closure != o.m_closure; }
-	bool operator< (const Delegate& o) const { return m_closure < o.m_closure; }
-	bool operator> (const Delegate& o) const { return m_closure > o.m_closure; }
+    bool operator== (const Delegate& o) const { return m_closure == o.m_closure; }
+    bool operator!= (const Delegate& o) const { return m_closure != o.m_closure; }
+    bool operator< (const Delegate& o) const { return m_closure < o.m_closure; }
+    bool operator> (const Delegate& o) const { return m_closure > o.m_closure; }
 
-	inline bool operator==(StaticFuncType func) {
-		return m_closure == func; }
-	inline bool operator!=(StaticFuncType func) {
-		return !operator==(func); }
+    inline bool operator==(StaticFuncType func) {
+        return m_closure == func; }
+    inline bool operator!=(StaticFuncType func) {
+        return !operator==(func); }
 
-	template <class X, class Y>
-	inline void bind(Y* pthis, RetType (X::* func)(Args...)) {
-		m_closure = { static_cast<X*>(pthis), func };
-	}
+    template <class X, class Y>
+    inline void bind(Y* pthis, RetType (X::* func)(Args...)) {
+        m_closure = { static_cast<X*>(pthis), func };
+    }
 
-	template <class X, class Y>
-	inline void bind(Y* pthis, RetType (X::* func)(Args...) const) {
-		m_closure = { static_cast<X*>(pthis), func };
-	}
+    template <class X, class Y>
+    inline void bind(Y* pthis, RetType (X::* func)(Args...) const) {
+        m_closure = { static_cast<X*>(pthis), func };
+    }
 
-	template <class X, class Y>
-	inline void bind(const Y* pthis, RetType (X::* func)(Args...) const) {
-		m_closure = { const_cast<X*>(pthis), func };
-	}
+    template <class X, class Y>
+    inline void bind(const Y* pthis, RetType (X::* func)(Args...) const) {
+        m_closure = { const_cast<X*>(pthis), func };
+    }
 
-	template <class X, class Y>
-	inline void bind(Y& p, RetType (X::* func)(Args...)) {
-		m_closure = { static_cast<X&>(p), func };
-	}
+    template <class X, class Y>
+    inline void bind(Y& p, RetType (X::* func)(Args...)) {
+        m_closure = { static_cast<X&>(p), func };
+    }
 
-	template <class X, class Y>
-	inline void bind(Y& p, RetType (X::* func)(Args...) const) {
-		m_closure = { static_cast<X&>(p), func };
-	}
+    template <class X, class Y>
+    inline void bind(Y& p, RetType (X::* func)(Args...) const) {
+        m_closure = { static_cast<X&>(p), func };
+    }
 
-	template <class X, class Y>
-	inline void bind(const Y& p, RetType (X::* func)(Args...) const) {
-		m_closure = { static_cast<Y&>(p), func };
-	}
+    template <class X, class Y>
+    inline void bind(const Y& p, RetType (X::* func)(Args...) const) {
+        m_closure = { static_cast<Y&>(p), func };
+    }
 
-	inline void bind(StaticFuncType func) {
-		m_closure = { this, &Delegate::InvokeStaticFunction, func };
-	}
+    inline void bind(StaticFuncType func) {
+        m_closure = { this, &Delegate::InvokeStaticFunction, func };
+    }
 
     inline Delegate& operator = (StaticFuncType func) {
-		bind(func);
-		return *this;
-	}
+        bind(func);
+        return *this;
+    }
 
 private:
-	RetType InvokeStaticFunction(Args ... args) const {
-		auto* func = m_closure.getStaticFunc();
-		return (*func)(args...);
-	}
+    RetType InvokeStaticFunction(Args ... args) const {
+        auto* func = m_closure.getStaticFunc();
+        return (*func)(args...);
+    }
 
-	ClosureType m_closure;
+    ClosureType m_closure;
 };
 
 template <class X, class Y, typename RetType, typename... Args>
 Delegate<RetType(Args...)> MakeDelegate(Y* x, RetType (X::*func)(Args...)) {
-	return Delegate<RetType(Args...)>(x, func);
+    return Delegate<RetType(Args...)>(x, func);
 }
 
 template <class X, class Y, typename RetType, typename... Args>
 Delegate<RetType(Args...)> MakeDelegate(Y* x, RetType (X::*func)(Args...) const) {
-	return Delegate<RetType(Args...)>(x, func);
+    return Delegate<RetType(Args...)>(x, func);
 }
 
 template <class X, class Y, typename RetType, typename... Args>
 Delegate<RetType(Args...)> MakeDelegate(const Y* x, RetType (X::*func)(Args...) const) {
-	return Delegate<RetType(Args...)>(x, func);
+    return Delegate<RetType(Args...)>(x, func);
 }
 
 template <typename RetType, typename... Args>
 Delegate<RetType(Args...)> MakeDelegate(RetType (* func)(Args...)) {
-	return Delegate<RetType(Args...)>(func);
+    return Delegate<RetType(Args...)>(func);
 }
 
 } // end delly namespace
